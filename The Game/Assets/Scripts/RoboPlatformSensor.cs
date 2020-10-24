f4e8d56189011e7c1295a3ec60f609b52b71fe67
Bryant <33697753+BryCant@users.noreply.github.com>
2020-10-23 23:24:55 -0500

    Robo start

--- The Game/Assets/Scripts/RoboPlatformSensor.cs
+++ The Game/Assets/Scripts/RoboPlatformSensor.cs
@@ -9,11 +9,21 @@ public class RoboPlatformSensor : MonoBehaviour
 
     public GameObject enemy_HealthBar;
     public GameObject enemyBody;
+    private Transform target;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Protagonist").GetComponent<Transform>();
+    }
 
     private void Update()
     {
-        enemyBody.transform.Translate(Vector2.left * speed * Time.deltaTime);
-        enemy_HealthBar.transform.position = new Vector3(enemyBody.transform.position.x, enemy_HealthBar.transform.position.y, enemy_HealthBar.transform.position.z);
+        if (Vector2.Distance(transform.position, target.position) > 5f)
+        {
+            enemyBody.transform.Translate(Vector2.left * speed * Time.deltaTime);
+            enemy_HealthBar.transform.position = new Vector3(enemyBody.transform.position.x, enemy_HealthBar.transform.position.y, enemy_HealthBar.transform.position.z);
+        }
+        
     }
 
     private void OnTriggerEnter2D(Collider2D other)
